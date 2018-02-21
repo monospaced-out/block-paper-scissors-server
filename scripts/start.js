5dabba1a9b1d3265eeebbd30d9944a39bfa5b1d0
Tyler Yasaka <hey@tyleryasaka.me>
2018-02-21 03:11:20 -0700

    Support mongodb connection

--- scripts/start.js
+++ scripts/start.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
+
 const express = require('express')
 const http = require('http')
-
 const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 
 const server = http.createServer(app)
 const io = require('socket.io')(server, {
@@ -9,9 +12,12 @@ const io = require('socket.io')(server, {
   pingInterval: 900
 })
 
-const DEFAULT_PORT = process.env.PORT || 8080;
+const dbName = process.env.DB_NAME
+const dbUrl = process.env.DB_URL
 
-let clients = [];
+const DEFAULT_PORT = process.env.PORT || 8080
+
+let clients = []
 
 io.on('connection', function (socket) {
   socket.on('introduction', function (introData) {
@@ -37,6 +43,46 @@ io.on('connection', function (socket) {
   })
 })
 
-server.listen(DEFAULT_PORT, function listening() {
-  console.log('Listening on %d', server.address().port);
+MongoClient.connect(dbUrl, (err, dbClient) => {
+  if (err) return console.log(err)
+  let db = dbClient.db(dbName)
+
+  app.use(bodyParser.json())
+
+  // CORS; https://enable-cors.org/server_expressjs.html
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+  })
+
+  app.post('/save-result', (req, res) => {
+    let key = String(req.body.key)
+    let gameId = req.body.gameId
+    let player = req.body.player
+    let opponent = req.body.opponent
+    db.collection('results').insertOne({ key, gameId, player, opponent }, (resultSaveErr) => {
+      if (!resultSaveErr) {
+        res.sendStatus(201)
+      } else {
+        console.log('error saving result', resultSaveErr)
+        res.sendStatus(500)
+      }
+    })
+  })
+
+  app.get('/results', (req, res) => {
+    db.collection('results').find().toArray((resultFetchErr, results) => {
+      if (!resultFetchErr) {
+        res.send(results)
+      } else {
+        console.log('error saving result', resultFetchErr)
+        res.sendStatus(500)
+      }
+    })
+  })
+
+  server.listen(DEFAULT_PORT, function listening() {
+    console.log('Listening on %d', server.address().port);
+  })
 })
